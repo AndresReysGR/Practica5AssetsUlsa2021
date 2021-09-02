0aefd4738eb7a045f0536084f6fda28f79e0d3e3
AndresReysGR <53828292+AndresReysGR@users.noreply.github.com>
2021-09-01 17:21:09 -0700

    Add files via upload

---
+++ Practica5Assets/Practica5Assets/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Practica5Assets
+//
+//  Created by Alumno on 9/1/21.
+//  Copyright © 2021 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var imgSegundaImagen: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imgSegundaImagen.image = UIImage(named: "A")
+    }
+
+
+}
+
